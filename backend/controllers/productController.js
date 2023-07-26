c2ba185820a9de19e850480954089f82db49c354
Aditya <aditya.kulkarni.it@gmail.com>
2023-07-26 20:56:54 +0530

    Implemented backend functionality to enable search, filtering, and pagination for improved data retrieval and user experience. The new features enhance the API's capabilities, allowing users to efficiently search for records, apply filters, and navigate through large datasets.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModels");
 const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures");
 
 // Create Product -- Admin
 exports.createProduct = catchAsyncErrors(async (req, res, next) => {
@@ -15,7 +16,16 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 
 // Get All Products
 exports.getAllProducts = catchAsyncErrors(async(req, res) => {
-    const products = await Product.find();
+
+    const resultPerPage = 5;
+    const productCount = await Product.countDocuments();
+
+
+    const apifeature = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage)
+    const products = await apifeature.query;
     res.status(200).json({
         success: true,
             products
@@ -31,7 +41,8 @@ exports.getProductDetails = catchAsyncErrors(async (req, res, next)=>{
     }
     res.status(200).json({
         success:true,
-        product
+        product,
+        productCount,
     })
 
 });
