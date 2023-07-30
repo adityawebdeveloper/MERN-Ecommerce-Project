a3012e9aaca27b40f0540b1bf8857c508ed2a683
Aditya <aditya.kulkarni.it@gmail.com>
2023-07-30 14:32:56 +0530

    Backend User & Password Authentication

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,14 +1,32 @@
 const express = require("express");
 
-const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productController"); // Import the controller function
+const {
+    getAllProducts,
+    createProduct,
+    updateProduct,
+    deleteProduct,
+    getProductDetails
+} = require("../controllers/productController"); // Import the controller function
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.route("/products").get(getAllProducts);
+router
+.route("/products")
+.get(getAllProducts);
 
 
-router.route("/products/new").post(createProduct);
+router
+.route("/products/new")
+.post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
 
-router.route("/products/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails)
+router
+.route("/products/:id")
+.put(isAuthenticatedUser,authorizeRoles("admin"), updateProduct)
+.delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct)
+.get(getProductDetails)
 
 module.exports = router
+
+
+  
